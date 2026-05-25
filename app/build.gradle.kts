dc0fa1ea6f207614778420825d03291815d4fff2
Gregoire L. D. <g@duval.paris>
2026-05-25 19:48:28 +0200

    New APK Name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.stashbase"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
