5be65e696de8d1fde06300eda7b78fa40a6dabd7
Gregoire L. D. <g@duval.paris>
2026-05-25 19:40:38 +0200

    New Auto-Build system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.stashbase"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
